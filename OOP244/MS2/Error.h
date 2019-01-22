2531e7950e5104ffd9ec82d15c445b57e10707ea
Melody Mastache <16945557+melodymastache@users.noreply.github.com>
2019-01-22 17:43:44 -0500

    Create Error.h

---
+++ OOP244/MS2/Error.h
@@ -0,0 +1,27 @@
+// Milestone 2
+// Name: Citlalli Melody Mastache
+// Student #: 126275189
+// Course: OOP244 Fall 2018
+// File: Error.h
+
+#ifndef AID_ERROR_H
+#define AID_ERROR_H
+#include <iostream>
+
+namespace aid {
+    class Error {
+        char * errorMessage;
+
+        public:
+        explicit Error(const char* error = nullptr);
+        Error(const Error& em) = delete;
+        Error& operator=(const Error& em) = delete;
+        virtual ~Error();
+        void clear();
+        bool isClear() const;
+        void message(const char*);
+        const char* message() const;
+    };
+    std::ostream& operator<<(std::ostream&, const Error&);
+}
+#endif
