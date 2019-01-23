fa92649699f3584cad3524221cfc097f47282a3a
Melody Mastache <16945557+melodymastache@users.noreply.github.com>
2019-01-22 19:59:03 -0500

    Create Tools.h

---
+++ OOP244/W1/athome/Tools.h
@@ -0,0 +1,16 @@
+// Workshop <Workshop #1>
+// Name: <Citlalli Melody Mastache>
+// Student #: <126275189>
+// Course: OOP244 Winter 2018
+// File: <tools.h>
+
+#ifndef NAMESPACE_TOOLS_H
+#define NAMESPACE_TOOLS_H
+namespace sict {
+// Displays the user interface menu
+int menu();
+
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
+}
+#endif
